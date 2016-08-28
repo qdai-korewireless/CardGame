c6a38f9baeba4f0dfb56b77cb4fd2cff50dca47e
Quan Dai <qdai@koresystemsgroup.com>
2016-08-28 13:55:02 -0500

    create basic card game 1. add test proj and reference FsUnit 2. create basic types for card game: Suit, Rank, Card, Deck, ShuffledDeck, Deal 3. simple methods and tests to get new deck, shuffle deck, deal card

---
+++ Game.Cards.Tests/DealingCardTests.fs
@@ -0,0 +1,18 @@
+﻿module Game.Cards.Tests.Dealing
+open Game.Cards
+open FsUnit
+open NUnit.Framework
+
+//Install-Package FsUnit -Version 1.4.1
+
+[<Test>]
+let ``should return 51 cards after dealing a card from a new deck`` () = 
+    let expected = 51
+    let actual = getNewDeck() |> shuffle |> dealCard |> fst |> getDeckOfShuffledDeck |> List.length
+    actual |> should equal expected
+
+[<Test>]
+let ``should return 0 card when dealing a card from depleted deck``() = 
+    let expected = 0
+    let actual = [] |> shuffle |> dealCard |> fst|> getDeckOfShuffledDeck |> List.length
+    actual |> should equal expected
---
+++ Game.Cards/Cards.fs
@@ -0,0 +1,38 @@
+﻿module Game.Cards
+
+type Suit = | Clubs| Diamonds| Hearts| Spades
+type Rank = 
+    | Two   | Three | Four
+    | Five  | Six   | Seven
+    | Eight | Nine  | Ten
+    | Jack  | Queen | King
+    | Ace
+type Card = Card of Suit*Rank
+type Deck = Card list
+type ShuffledDeck = ShuffledDeck of Deck
+type Deal = Deal of ShuffledDeck*Card option
+
+let getAllSuits() =
+    [Clubs;Diamonds;Hearts;Spades]
+let getAllRanks() = 
+    [Two;Three;Four;Five;Six;Seven;Eight;Nine;Ten;Jack;Queen;King;Ace]
+
+let getNewDeck() = 
+    let suits = getAllSuits()
+    let ranks = getAllRanks()
+    [for s in suits do
+        for r in ranks do
+            yield Card(s,r)]
+
+let shuffle deck = 
+    let rd = new System.Random(10000)
+    deck |> List.sortBy(fun _ -> rd.Next()) |> ShuffledDeck
+
+
+let dealCard deck = 
+    match deck with
+    | ShuffledDeck(top::rest) -> ShuffledDeck(rest),Some top
+    | ShuffledDeck [] -> ShuffledDeck([]),None
+
+let getDeckOfShuffledDeck (ShuffledDeck deck) =
+    deck
