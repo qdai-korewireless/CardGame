980f8a2a115d68dca220e5662f68b0bc0d1f37ef
Quan Dai <qdai@koresystemsgroup.com>
2016-08-30 09:31:14 -0500

    add one pair rule fix some other testing data

--- Game.Cards.Tests/EvaluateHandTests.fs
+++ Game.Cards.Tests/EvaluateHandTests.fs
@@ -9,13 +9,30 @@ open Game.PlayPoker
 [<Test>]
 let ``should player 2 win due to high hand``() = 
     let player1 = {Name="Player 1";
-                    Hand=[Card(Diamonds, Ten);Card(Diamonds, Nine);Card(Diamonds, Eight);Card(Diamonds, Seven);Card(Spades, Ten)];
+                    Hand=[Card(Diamonds, Ten);Card(Diamonds, Nine);Card(Diamonds, Eight);Card(Diamonds, Seven);Card(Spades, Three)];
                     Position = 1}
     let player2 = {Name="Player 2";
                     Hand=[Card(Diamonds, Queen);Card(Diamonds, Nine);Card(Diamonds, Eight);Card(Diamonds, Seven);Card(Hearts, Ace)];
                     Position = 2}
     let player3 = {Name="Player 3";
                     Hand=[Card(Diamonds, Queen);Card(Diamonds, Nine);Card(Diamonds, Six);Card(Diamonds, Seven);Card(Hearts, Two)];
+                    Position = 3}
+    let expected = [player2;player3;player1]
+
+    let actual = evaluate [player1;player2;player3]
+
+    actual |> should equal expected
+
+[<Test>]
+let ``should player 2 win due to one pair``() = 
+    let player1 = {Name="Player 1";
+                    Hand=[Card(Diamonds, Five);Card(Diamonds, Ace);Card(Spades, Two);Card(Diamonds, King);Card(Diamonds, Three)];
+                    Position = 1}
+    let player2 = {Name="Player 2";
+                    Hand=[Card(Diamonds, Five);Card(Clubs, Five);Card(Spades, Ace);Card(Diamonds, Jack);Card(Hearts, Two)];
+                    Position = 2}
+    let player3 = {Name="Player 3";
+                    Hand=[Card(Hearts, King);Card(Spades, Five);Card(Clubs, Three);Card(Hearts, Six);Card(Hearts, Ace)];
                     Position = 3}
     let expected = [player2;player3;player1]
 
@@ -34,7 +51,7 @@ let ``should player 2 win due to 3 of kind``() =
     let player3 = {Name="Player 3";
                     Hand=[Card(Diamonds, Queen);Card(Diamonds, Nine);Card(Diamonds, Six);Card(Diamonds, Seven);Card(Hearts, Two)];
                     Position = 3}
-    let expected = [player2;player3;player1]
+    let expected = [player2;player1;player3]
 
     let actual = evaluate [player1;player2;player3]
 
--- Game.Cards/PlayPoker.fs
+++ Game.Cards/PlayPoker.fs
@@ -123,11 +123,30 @@ let fullHouseRule player1 player2 =
                 0
     else 
         0
-
+//Rule: One pair
+let onePairRule player1 player2 = 
+    let g1 = groupCardsByRank player1 |> Seq.toList
+    let g2 = groupCardsByRank player2 |> Seq.toList
+    let p1IsOP = g1 |> Seq.length = 4 && (fst g1.[0]) = 2
+    let p2IsOP = g2 |> Seq.length = 4 && (fst g2.[0]) = 2
+    if p1IsOP && not p2IsOP then
+        -1
+    else if p2IsOP && not p1IsOP then
+        1
+    else
+        let p1c1 = rankScore (snd g1.[0])
+        let p2c1 = rankScore (snd g2.[0])
+        if p1c1 > p2c1 then
+            -1
+        else if p1c1 < p2c1 then
+            1
+        else
+            0
 
 let evaluate players =
     players 
     |> sortWithRule highHandRule 
+    |> sortWithRule onePairRule
     |> sortWithRule threeOfKindRule 
     |> sortWithRule flushRule 
     |> sortWithRule fullHouseRule 
